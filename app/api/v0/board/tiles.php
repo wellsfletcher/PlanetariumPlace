a714156d3f89dd8739d60e534e48b980e6466dee
Fletcher Wells <wellsfletcher@gmail.com>
2021-01-05 22:28:10 -0500

    Fixed php issue.

--- app/api/v0/board/tiles.php
+++ app/api/v0/board/tiles.php
@@ -7,6 +7,8 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once '../config/database.php';
 include_once '../objects/board.php';
 
+// this file is located at http://planetarium.place/api/v0/board/tiles.php
+
 // instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
--- app/api/v0/config/database.php
+++ app/api/v0/config/database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
-    // private $host = "localhost";
-    private $host = "127.0.0.1";
+    private $host = "localhost";
+    // private $host = "127.0.0.1";
     // private $db_name = "myDBName";
     // private $username = "fakeUserName69";
     // private $password = "mnbvcxz123456789mnbvcxz";
@@ -17,9 +17,9 @@ class Database {
             // $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             // $this->conn->exec("set names utf8");
             $this->conn = new Redis();
-            echo "host = " . $this->$host . "\n";
-            echo "port = " . $this->$port . "\n";
-            $this->conn->connect($this->$host, $this->$port);
+            echo "host = " . $this->host . "\n";
+            echo "port = " . $this->port . "\n";
+            $this->conn->connect($this->host, $this->port);
         } catch(Exception $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
