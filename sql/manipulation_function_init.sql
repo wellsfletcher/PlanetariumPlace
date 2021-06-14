026d685fb42dfaf180d3f5b1ce4782b613a704ee
wellsfletcher <wellsfletcher@gmail.com>
2021-06-13 21:39:30 -0400

    Limited the number of history items.

--- sql/manipulation_function_init.sql
+++ sql/manipulation_function_init.sql
@@ -10,6 +10,27 @@ BEGIN
 
     insert into TileHistory(board_id, tile_index, color, placement_time) VALUES(board_id, tile_index, color, NOW(3));
     -- set @box_id = (select max(id) from Box);
+    -- delete rows if the tile history exceeds the maximum amount of rows
+    /*
+    delete
+    from TileHistory
+    where id not in (
+		select id
+        from TileHistory
+        order by placement_time DESC
+        LIMIT 2048
+    );
+    */
+    delete th1
+	from TileHistory as th1
+	left outer join (
+		select id
+		from TileHistory
+		order by placement_time DESC
+		LIMIT 2048
+	) as th2
+	on th1.id = th2.id
+	where th2.id is null;
 
 -- End of solution
 END //
--- sql/tester.sql
+++ sql/tester.sql
@@ -5,7 +5,46 @@ select SUBTIME("10:24:21.125", ".2");
 call append_tile(1,  512, 4);
 
 select * from TileHistory;
+select count(*) from TileHistory;
 
 -- select query_recent( NOW );
 # I'm gonna have to just call the contents of this query from the php most likely... or modify the procedure to only select what is needed
 call query_recent(1, five_seconds_ago());
+
+/*
+delete 
+from TileHistory
+where id not in (
+	select id
+	from TileHistory
+	order by placement_time DESC
+	LIMIT 30
+);
+*/
+
+/*
+select * 
+from TileHistory as th1
+inner join (
+	select id
+	from TileHistory
+	order by placement_time DESC
+	LIMIT 30
+) as th2
+on th1.id = th2.id;
+*/
+
+/* --
+delete th1
+from TileHistory as th1
+left outer join (
+	select id
+	from TileHistory
+	order by placement_time DESC
+	LIMIT 30
+) as th2
+on th1.id = th2.id
+where th2.id is null;
+*/
+
+
