d60f55ad2c2ab7fc957da9b85ef18325fd689534
wellsfletcher <wellsfletcher@gmail.com>
2021-06-27 21:05:17 -0400

    Fixed CORS issue

--- app/api/v0/board/draw.php
+++ app/api/v0/board/draw.php
@@ -2,9 +2,11 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+// header("Access-Control-Allow-Methods: POST");
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
 // include database and object files
 include_once '../config/database.php';
